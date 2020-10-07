4f078bb2a33c0a201befb010722d35aba571e36c
Richard Beard <tazitelo@gmail.com>
2020-10-07 11:59:49 +0200

    Minor polish and compatibility improvement
    - Tabified all the spaces
    - Removed double empty lines
    - Fixed some code and comment inconsistencies
    - Removed the servertest.ini renaming for compatibility reasons

--- ProjectZomboid.cs/ProjectZomboid.cs
+++ ProjectZomboid.cs/ProjectZomboid.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Text;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Engine;
@@ -10,137 +9,121 @@ using WindowsGSM.GameServer.Query;
 
 namespace WindowsGSM.Plugins
 {
-    public class ProjectZomboid : SteamCMDAgent // SteamCMDAgent is used because Project Zomboid relies on SteamCMD for installation and update process
-    {
-        // - Plugin Details
-        public Plugin Plugin = new Plugin
-        {
-            name = "WindowsGSM.ProjectZomboid", // WindowsGSM.XXXX
-            author = "Beard",
-            description = "🧩 WindowsGSM plugin for supporting Project Zomboid Dedicated Server",
-            version = "1.0",
-            url = "https://github.com/DoctorBeardz/WindowsGSM.ProjectZomboid", // Github repository link (Best practice)
-            color = "#38CDD4" // Color Hex
-        };
+	public class ProjectZomboid : SteamCMDAgent // SteamCMDAgent is used because Project Zomboid relies on SteamCMD for installation and update process
+	{
+		// - Plugin Details
+		public Plugin Plugin = new Plugin
+		{
+			name = "WindowsGSM.ProjectZomboid", // WindowsGSM.XXXX
+			author = "Beard",
+			description = "🧩 WindowsGSM plugin for supporting Project Zomboid Dedicated Server",
+			version = "1.2",
+			url = "https://github.com/DoctorBeardz/WindowsGSM.ProjectZomboid", // Github repository link (Best practice)
+			color = "#38CDD4" // Color Hex
+		};
 
+		// - Standard Constructor and properties
+		public ProjectZomboid(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+		private readonly ServerConfig _serverData; // Store server start metadata, such as start ip, port, start param, etc
 
-        // - Standard Constructor and properties
-        public ProjectZomboid(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
-        private readonly ServerConfig _serverData; // Store server start metadata, such as start ip, port, start param, etc
+		// - Settings properties for SteamCMD installer
+		public override bool loginAnonymous => true; // Project Zomboid does not require a steam account to install the server, so loginAnonymous = false
+		public override string AppId => "380870"; // Game server appId, Project Zomboid is 380870
 
+		// - Game server Fixed variables
+		public override string StartPath => "ProjectZomboid64.exe"; // Game server start path, for Project Zomboid, it is StartServer64.bat
+		public string FullName = "Project Zomboid Server"; // Game server FullName
+		public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+		public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
+		public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
-        // - Settings properties for SteamCMD installer
-        public override bool loginAnonymous => true; // Project Zomboid does not require a steam account to install the server, so loginAnonymous = false
-        public override string AppId => "380870"; // Game server appId, Project Zomboid is 380870
+		// - Game server default values
+		public string Port = "16261"; // Default port
+		public string QueryPort = "16262"; // Default query port
+		public string Defaultmap = "Muldraugh, KY"; // Default map name
+		public string Maxplayers = "64"; // Default maxplayers
+		public string Additional = "-cachedir=.\\Zomboid"; // Additional server start parameter
 
+		// - Create a default cfg for the game server after installation
+		public async void CreateServerCFG() { }
 
-        // - Game server Fixed variables
-        public override string StartPath => "ProjectZomboid64.exe"; // Game server start path, for Project Zomboid, it is StartServer64.bat
-        public string FullName = "Project Zomboid Server"; // Game server FullName
-        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
-        public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
-        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+		// - Start server function, return its Process to WindowsGSM
+		public async Task<Process> Start()
 
+		{
+			// Prepare start parameter
+			var param = new StringBuilder($"{_serverData.ServerParam} -port {_serverData.ServerPort}");
 
-        // - Game server default values
-        public string Port = "16261"; // Default port
-        public string QueryPort = "16262"; // Default query port
-        public string Defaultmap = "Muldraugh, KY"; // Default map name
-        public string Maxplayers = "64"; // Default maxplayers
-        public string Additional = "-cachedir=.\\Zomboid"; // Additional server start parameter
+			// Prepare Process
+			var p = new Process
+			{
+				StartInfo =
+				{
+					WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+					FileName = ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath),
+					Arguments = param.ToString(),
+					WindowStyle = ProcessWindowStyle.Minimized,
+					UseShellExecute = false
+				},
+				EnableRaisingEvents = true
+			};
 
+			// Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+			if (AllowsEmbedConsole)
+			{
+				p.StartInfo.CreateNoWindow = true;
+				p.StartInfo.RedirectStandardInput = true;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.RedirectStandardError = true;
+				var serverConsole = new ServerConsole(_serverData.ServerID);
+				p.OutputDataReceived += serverConsole.AddOutput;
+				p.ErrorDataReceived += serverConsole.AddOutput;
 
-        // - Create a default cfg for the game server after installation
-        public async void CreateServerCFG()
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine($"PublicName={_serverData.ServerName}");
-            sb.AppendLine($"DefaultPort={_serverData.ServerPort}");
-            sb.AppendLine($"RCONPassword=={ _serverData.GetRCONPassword()}");
-            File.WriteAllText(ServerPath.GetServersServerFiles(_serverData.ServerID, "servertest"), sb.ToString());
-        }
+				// Start Process
+				try
+				{
+					p.Start();
+				}
+				catch (Exception e)
+				{
+					Error = e.Message;
+					return null; // return null if fail to start
+				}
 
+				p.BeginOutputReadLine();
+				p.BeginErrorReadLine();
+				return p;
+			}
 
-        // - Start server function, return its Process to WindowsGSM
-        public async Task<Process> Start()
-        {
-            // Prepare start parameter
-            var param = new StringBuilder($"{_serverData.ServerParam} -servername {_serverData.ServerName} -port {_serverData.ServerPort}");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+			// Start Process
+			try
+			{
+				p.Start();
+				return p;
+			}
+			catch (Exception e)
+			{
+				Error = e.Message;
+				return null; // return null if fail to start
+			}
+		}
 
-
-            // Prepare Process
-            var p = new Process
-            {
-                StartInfo =
-                {
-                    WindowStyle = ProcessWindowStyle.Minimized,
-                    UseShellExecute = false,
-                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
-                    FileName = ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath),
-                    Arguments = param.ToString()
-                },
-                EnableRaisingEvents = true
-            };
-
-
-            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
-            if (AllowsEmbedConsole)
-            {
-                p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.RedirectStandardInput = true;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
-                var serverConsole = new ServerConsole(_serverData.ServerID);
-                p.OutputDataReceived += serverConsole.AddOutput;
-                p.ErrorDataReceived += serverConsole.AddOutput;
-
-                // Start Process
-                try
-                {
-                    p.Start();
-                }
-                catch (Exception e)
-                {
-                    Error = e.Message;
-                    return null; // return null if fail to start
-                }
-
-                p.BeginOutputReadLine();
-                p.BeginErrorReadLine();
-                return p;
-            }
-
-            // Start Process
-            try
-            {
-                p.Start();
-                return p;
-            }
-            catch (Exception e)
-            {
-                Error = e.Message;
-                return null; // return null if fail to start
-            }
-        }
-
-
-        // - Stop server function
-        public async Task Stop(Process p)
-        {
-            await Task.Run(() =>
-            {
-                if (p.StartInfo.RedirectStandardInput)
-                {
-                    // Send "stop" command to StandardInput stream if EmbedConsole is on
-                    p.StandardInput.WriteLine("quit");
-                }
-                else
-                {
-                    // Send "stop" command to game server process MainWindow
-                    ServerConsole.SendMessageToMainWindow(p.MainWindowHandle, "quit");
-                }
-            });
-        }
-    }
+		// - Stop server function
+		public async Task Stop(Process p)
+		{
+			await Task.Run(() =>
+			{
+				if (p.StartInfo.RedirectStandardInput)
+				{
+					// Send "quit" command to StandardInput stream if EmbedConsole is on
+					p.StandardInput.WriteLine("quit");
+				}
+				else
+				{
+					// Send "quit" command to game server process MainWindow
+					ServerConsole.SendMessageToMainWindow(p.MainWindowHandle, "quit");
+				}
+			});
+		}
+	}
 }
