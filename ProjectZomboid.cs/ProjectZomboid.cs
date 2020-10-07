8d8b2f1d9a129a481f017fe35b1e236cfb82ecff
Richard Beard <tazitelo@gmail.com>
2020-10-07 18:27:59 +0200

    Fixed the query port

--- ProjectZomboid.cs/ProjectZomboid.cs
+++ ProjectZomboid.cs/ProjectZomboid.cs
@@ -17,7 +17,7 @@ namespace WindowsGSM.Plugins
 			name = "WindowsGSM.ProjectZomboid", // WindowsGSM.XXXX
 			author = "Beard",
 			description = "🧩 WindowsGSM plugin for supporting Project Zomboid Dedicated Server",
-			version = "1.2",
+			version = "1.3",
 			url = "https://github.com/DoctorBeardz/WindowsGSM.ProjectZomboid", // Github repository link (Best practice)
 			color = "#38CDD4" // Color Hex
 		};
@@ -34,12 +34,12 @@ namespace WindowsGSM.Plugins
 		public override string StartPath => "ProjectZomboid64.exe"; // Game server start path, for Project Zomboid, it is StartServer64.bat
 		public string FullName = "Project Zomboid Server"; // Game server FullName
 		public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
-		public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
+		public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
 		public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
 		// - Game server default values
 		public string Port = "16261"; // Default port
-		public string QueryPort = "16262"; // Default query port
+		public string QueryPort = "16261"; // Default query port
 		public string Defaultmap = "Muldraugh, KY"; // Default map name
 		public string Maxplayers = "64"; // Default maxplayers
 		public string Additional = "-cachedir=.\\Zomboid"; // Additional server start parameter
