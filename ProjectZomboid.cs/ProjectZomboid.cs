94bdfd82a74720e5cbde7349678f5197f91dadef
raziel7893 <raziel7893@gmail.com>
2026-03-19 10:41:50 +0100

    fix java paths as the old variant didn't seem to work anymore

--- ProjectZomboid.cs/ProjectZomboid.cs
+++ ProjectZomboid.cs/ProjectZomboid.cs
@@ -71,12 +71,16 @@ namespace WindowsGSM.Plugins
             //if you have Memory issues you can try to edit -Xms16g -Xmx16g to better suite your system
             param.Append(" -Xms16g -Xmx16g \"-Djava.library.path=natives/;natives/win64/;.\" \"-Dstatistic=0\"");
             //java classpath copied from startScript
-            param.Append(" -cp \"java/istack-commons-runtime.jar;java/jassimp.jar;java/javacord-2.0.17-shaded.jar;" +
+            param.Append(" -cp \"java/;java/projectzomboid.jar/\"");
+
+            //old paths, didn't work for new versions anymore, so i leave it in as a precaution
+            /*            param.Append(" -cp \"java/istack-commons-runtime.jar;java/jassimp.jar;java/javacord-2.0.17-shaded.jar;" +
                 "java/javax.activation-api.jar;java/jaxb-api.jar;java/jaxb-runtime.jar;java/lwjgl.jar;" +
                 "java/lwjgl-natives-windows.jar;java/lwjgl-glfw.jar;java/lwjgl-glfw-natives-windows.jar;" +
                 "java/lwjgl-jemalloc.jar;java/lwjgl-jemalloc-natives-windows.jar;java/lwjgl-opengl.jar;" +
                 "java/lwjgl-opengl-natives-windows.jar;java/lwjgl_util.jar;java/sqlite-jdbc-3.27.2.1.jar;" +
-                "java/trove-3.0.3.jar;java/uncommons-maths-1.2.3.jar;java/commons-compress-1.18.jar;java/\"");
+                "java/trove-3.0.3.jar;java/uncommons-maths-1.2.3.jar;java/commons-compress-1.18.jar;java/\"");*/
+
             //actual start class
             param.Append(" zombie.network.GameServer");
             //add custom parameters and ports
